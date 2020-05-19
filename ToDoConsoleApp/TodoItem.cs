0521bf6395a18c8a8ac3ff9b70a2755dc29b048f
Ronald Ogando Negron <ronaldogando150@gmail.com>
2020-05-19 12:47:47 -0400

    ToDoItem class added

---
+++ ToDoConsoleApp/TodoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace ToDoConsoleApp
+{
+    class TodoItem
+    {
+        private string title;
+        private string description;
+        private bool completed;
+
+        public string Title 
+        { 
+            get { return title; } 
+        }
+
+        public string Description
+        {
+            get { return description; }
+        }
+
+        public bool Completed
+        {
+            get { return completed; }
+        }
+
+        TodoItem(string title, string description, bool completed)
+        {
+            this.title = title;
+            this.description = description;
+            this.completed = completed;
+        }
+    }
+}
